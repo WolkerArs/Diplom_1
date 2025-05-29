0abe7ae7ec2d4cd2698189b92e13608f10e02dc7
Arseny Dunaev <WolkerArs@yandex.ru>
2025-05-29 23:36:42 +0300

    Созданы файлы для теста + обновлен ingnore файл

--- praktikum_files/burger.py
+++ praktikum_files/burger.py
@@ -1,7 +1,7 @@
 from typing import List
 
-from praktikum.bun import Bun
-from praktikum.ingredient import Ingredient
+from praktikum_files.bun import Bun
+from praktikum_files.ingredient import Ingredient
 
 
 class Burger:
